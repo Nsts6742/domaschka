29c20514e937228a137b7d219b9270c6ca2ab05a
Nsts6742 <ryzhiy.2020@list.ru>
2023-10-15 17:03:00 +0300

    2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+string digit = Console.ReadLine();
+int number;
+number = int.Parse(digit);
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine("Третья цифра - " + thirdDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры не существует.");
+}
