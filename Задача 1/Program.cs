43ff276e705c3e554b7c0554effe6e547e3696f6
Nsts6742 <ryzhiy.2020@list.ru>
2023-10-15 17:01:18 +0300

    1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+string digit = Console.ReadLine();
+int number;
+number = int.Parse(digit);
+if (number > 99 & number < 1000) 
+{
+    int oneAndSecondNumber = number / 10;
+    int secondNumber = oneAndSecondNumber % 10;
+    Console.WriteLine("Вторая цифра числа - " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Неверно введено число.");
+}
